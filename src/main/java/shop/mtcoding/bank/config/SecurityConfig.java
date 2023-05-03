a9c8b1adc934a480de17e532fa0697a586cc0437
yoongdoo <yoong.n@krustuniverse.com>
2023-05-03 16:25:57 +0900

    add SecurityConfig

---
+++ src/main/java/shop/mtcoding/bank/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package shop.mtcoding.bank.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import shop.mtcoding.bank.domain.user.UserEnum;
+
+@Configuration
+public class SecurityConfig {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        log.debug("디버그 : BCryptPasswordEncoder 빈 등록됨");
+        return new BCryptPasswordEncoder();
+    }
+
+    // JWT 서버 구축 (Session 사용 X)
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();    // iframe 허용 안 함
+        http.csrf().disable();  // enable이면 post맨 작동 안 함
+        http.cors().configurationSource(configurationSource());
+
+        //  jSessionId 서버에서 관리 안 함
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //  react, 앱으로 요청
+        http.formLogin().disable();
+        //  브라우저가 팝업창을 이용해서 사용자 인증 진행 X
+        http.httpBasic().disable();
+        http.authorizeRequests()
+            .antMatchers("/api/s/**").authenticated()
+            .antMatchers("api/admin/**").hasRole("" + UserEnum.ADMIN)
+            .anyRequest().permitAll();
+
+        return http.build();
+    }
+
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE (Javascript 요청 허용)
+        configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 (프론트엔드 IP만 허용)
+        configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
