f1bfe4c5da2d54c6c1e84300667539408c786a83
yoongdoo <yoong.n@krustuniverse.com>
2023-05-04 11:27:56 +0900

    add UserController

--- src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
+++ src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
@@ -6,14 +6,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserEnum;
 
+import javax.validation.constraints.NotEmpty;
+
 public class UserReqDto {
 
     @Setter
     @Getter
     public static class JoinReqDto {
+        @NotEmpty // null이거나 공백일 수 없음
         private String username;
+        @NotEmpty
         private String password;
+        @NotEmpty
         private String email;
+        @NotEmpty
         private String fullname;
 
         public User toEntity(BCryptPasswordEncoder passwordEncoder) {
---
+++ src/main/java/shop/mtcoding/bank/web/UserController.java
@@ -0,0 +1,43 @@
+package shop.mtcoding.bank.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import shop.mtcoding.bank.dto.ResponseDto;
+import shop.mtcoding.bank.dto.user.UserReqDto.JoinReqDto;
+import shop.mtcoding.bank.dto.user.UserRespDto.JoinRespDto;
+import shop.mtcoding.bank.service.UserService;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public ResponseEntity join(@RequestBody @Valid JoinReqDto joinReqDto, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(new ResponseDto<>(-1, "유효성 검사 실패", errorMap), HttpStatus.BAD_REQUEST);
+        }
+
+        JoinRespDto joinRespDto = userService.회원가입(joinReqDto);
+        return new ResponseEntity<>(new ResponseDto<>(1, "회원가입 성공", joinRespDto), HttpStatus.CREATED);
+    }
+}
