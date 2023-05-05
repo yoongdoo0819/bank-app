809b5a376a9cc44f6f17fe40659ecd7220eacec8
yoongdoo <yoong.n@krustuniverse.com>
2023-05-05 20:42:47 +0900

    deleteAccount (AccountService Test)

--- src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
+++ src/test/java/shop/mtcoding/bank/service/AccountServiceTest.java
@@ -15,10 +15,12 @@ import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserRepository;
 import shop.mtcoding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
 import shop.mtcoding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
+import shop.mtcoding.bank.handler.ex.CustomApiException;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -61,5 +63,22 @@ class AccountServiceTest extends DummyObject {
 
         // then
         assertThat(accountSaveRespDto.getNumber()).isEqualTo(1111L);
+    }
+
+    @Test
+    void 계좌삭제_test() throws Exception{
+        // given
+        Long number = 1111L;
+        Long userId = 2L;
+
+        // stub
+        User ssar = newMockUser(1L, "ssar", "쌀");
+        Account ssarAccount = newMockAccount(1L, 1111L, 1000L, ssar);
+        when(accountRepository.findByNumber(any())).thenReturn(Optional.of(ssarAccount));
+
+        // when
+        assertThrows(CustomApiException.class, () -> accountService.계좌삭제(number, userId));
+
+        // then
     }
 }
