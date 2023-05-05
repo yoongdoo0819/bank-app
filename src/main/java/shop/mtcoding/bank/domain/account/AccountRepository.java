7ba71b02c804311fe4b11fec1355bcfe89867df5
yoongdoo <yoong.n@krustuniverse.com>
2023-05-05 17:08:00 +0900

    add AccountService

--- src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -2,5 +2,8 @@ package shop.mtcoding.bank.domain.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByNumber(Long number);
 }
---
+++ src/main/java/shop/mtcoding/bank/service/AccountService.java
@@ -0,0 +1,80 @@
+package shop.mtcoding.bank.service;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.handler.ex.CustomApiException;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class AccountService {
+    private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
+
+    @Transactional
+    public AccountSaveRespDto 계좌등록(AccountSaveReqDto accountSaveReqDto, Long userId) {
+        // User가 DB에 있는지 검증
+        User userPS = userRepository.findById(userId).orElseThrow(
+                () -> new CustomApiException("유저를 찾을 수 없습니다")
+        );
+
+        // 해당 계좌가 DB에 있는지 중복 여부를 체크
+        Optional<Account> accountOP = accountRepository.findByNumber(accountSaveReqDto.getNumber());
+        if (accountOP.isPresent()) {
+            throw new CustomApiException("해당 계좌가 이미 존재합니다");
+        }
+
+        // 계좌 등록
+        Account accountPS = accountRepository.save(accountSaveReqDto.toEntity(userPS));
+
+        // DTO를 응답
+        return new AccountSaveRespDto(accountPS);
+    }
+
+    @Getter
+    @Setter
+    public static class AccountSaveRespDto {
+        private Long id;
+        private Long number;
+        private Long balance;
+
+        public AccountSaveRespDto(Account account) {
+            this.id = account.getId();
+            this.number = account.getNumber();
+            this.balance = account.getBalance();
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class AccountSaveReqDto {
+
+        @NotNull
+        @Digits(integer = 4, fraction = 4)
+        private Long number;
+
+        @NotNull
+        @Digits(integer = 4, fraction = 4)
+        private Long password;
+
+        public Account toEntity(User user) {
+            return Account.builder()
+                    .number(number)
+                    .password(password)
+                    .balance(1000L)
+                    .user(user)
+                    .build();
+        }
+    }
+}
