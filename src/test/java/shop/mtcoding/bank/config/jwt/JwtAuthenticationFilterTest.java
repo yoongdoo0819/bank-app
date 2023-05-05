0657db5fdda203a28339a080110c59f1e4b895a0
yoongdoo <yoong.n@krustuniverse.com>
2023-05-05 16:02:06 +0900

    successfulAuthentication Test

---
+++ src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -0,0 +1,71 @@
+package shop.mtcoding.bank.config.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import shop.mtcoding.bank.config.dummy.DummyObject;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.dto.user.UserReqDto.LoginReqDto;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class JwtAuthenticationFilterTest extends DummyObject {
+
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.save(newUser("ssar", "쌀"));
+    }
+
+    @Test
+    void successfulAuthentication_test() throws Exception{
+        // given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("1234");
+        String requestBody = om.writeValueAsString(loginReqDto);
+        System.out.println("requestBody = " + requestBody);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/api/login").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+        System.out.println("responseBody = " + responseBody);
+        System.out.println("jwtToken = " + jwtToken);
+
+        // then
+        resultActions.andExpect(status().isOk());
+        assertNotNull(jwtToken);
+        assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
+        resultActions.andExpect(jsonPath("$.data.username").value("ssar"));
+    }
+
+    @Test
+    void unsuccessfulAuthentication_test() {
+        // given
+
+        // when
+
+        // then
+    }
+}
