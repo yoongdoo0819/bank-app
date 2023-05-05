d8dd8e73c3fdb519d226a35b00cdc7f2e6384f8c
yoongdoo <yoong.n@krustuniverse.com>
2023-05-05 20:15:25 +0900

    add findUserAccount api

--- src/main/java/shop/mtcoding/bank/web/AccountController.java
+++ src/main/java/shop/mtcoding/bank/web/AccountController.java
@@ -5,15 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.bank.config.auth.LoginUser;
 import shop.mtcoding.bank.dto.ResponseDto;
 import shop.mtcoding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
 import shop.mtcoding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
 import shop.mtcoding.bank.service.AccountService;
+import shop.mtcoding.bank.service.AccountService.AccountListRespDto;
 
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -30,5 +28,31 @@ public class AccountController {
 
         AccountSaveRespDto accountSaveRespDto = accountService.계좌등록(accountSaveReqDto, loginUser.getUser().getId());
         return new ResponseEntity<>(new ResponseDto<>(1, "계좌등록 성공", accountSaveRespDto), HttpStatus.CREATED);
+    }
+
+
+    /**
+     * findUserAccount 동일 사용자임을 확인하는 로직이 존재
+    @GetMapping("/s/account/{id}")
+    public ResponseEntity<?> findUserAccount(@PathVariable Long id, @AuthenticationPrincipal LoginUser loginUser) {
+
+        // 권한처리를 해야만 하기 때문에 아래 로직이 반드시 필요
+        if (id != loginUser.getUser().getId()) {
+            throw new CustomForbiddenException("권한이 없습니다");
+        }
+
+        AccountListRespDto accountListRespDto = accountService.계좌목록보기_유저별(id);
+        return new ResponseEntity<>(new ResponseDto<>(1, "계좌목록보기_유저별 성공", accountListRespDto), HttpStatus.OK);
+    }
+     */
+
+    /**
+     * findUserAccount 동일 사용자임을 확인하는 로직이 존재하지 않음
+     */
+    @GetMapping("/s/account/login-user")
+    public ResponseEntity<?> findUserAccount(@AuthenticationPrincipal LoginUser loginUser) {
+
+        AccountListRespDto accountListRespDto = accountService.계좌목록보기_유저별(loginUser.getUser().getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "계좌목록보기_유저별 성공", accountListRespDto), HttpStatus.OK);
     }
 }
