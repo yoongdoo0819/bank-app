51276cdd1e5e13972e6353fb61f12592508340e4
yoongdoo <yoong.n@krustuniverse.com>
2023-05-06 21:38:31 +0900

    dynamic query Test

--- src/main/java/shop/mtcoding/bank/config/dummy/DummyObject.java
+++ src/main/java/shop/mtcoding/bank/config/dummy/DummyObject.java
@@ -2,6 +2,7 @@ package shop.mtcoding.bank.config.dummy;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
 import shop.mtcoding.bank.domain.transaction.Transaction;
 import shop.mtcoding.bank.domain.transaction.TransactionEnum;
 import shop.mtcoding.bank.domain.user.User;
@@ -77,6 +78,69 @@ public class DummyObject {
                 .updatedAt(LocalDateTime.now())
                 .build();
 
+        return transaction;
+    }
+
+    protected Transaction newWithdrawTransaction(Account account, AccountRepository accountRepository) {
+        account.withdraw(100L); // 1000원이 있었다면 900원이 됨
+
+        // Repository Test에서는 더티체킹 됨
+        // Controller Test에서는 더티체킹 안됨
+        if (accountRepository != null) {
+            accountRepository.save(account);
+        }
+        Transaction transaction = Transaction.builder()
+                .withdrawAccount(account)
+                .depositAccount(null)
+                .withdrawAccountBalance(account.getBalance())
+                .depositAccountBalance(null)
+                .amount(100L)
+                .gubun(TransactionEnum.WITHDRAW)
+                .sender(account.getNumber() + "")
+                .receiver("ATM")
+                .build();
+        return transaction;
+    }
+
+    protected Transaction newDepositTransaction(Account account, AccountRepository accountRepository) {
+        account.deposit(100L); // 1000원이 있었다면 900원이 됨
+        // 더티체킹이 안되기 때문에
+        if (accountRepository != null) {
+            accountRepository.save(account);
+        }
+        Transaction transaction = Transaction.builder()
+                .withdrawAccount(null)
+                .depositAccount(account)
+                .withdrawAccountBalance(null)
+                .depositAccountBalance(account.getBalance())
+                .amount(100L)
+                .gubun(TransactionEnum.DEPOSIT)
+                .sender("ATM")
+                .receiver(account.getNumber() + "")
+                .tel("01022227777")
+                .build();
+        return transaction;
+    }
+
+    protected Transaction newTransferTransaction(Account withdrawAccount, Account depositAccount,
+                                                 AccountRepository accountRepository) {
+        withdrawAccount.withdraw(100L);
+        depositAccount.deposit(100L);
+        // 더티체킹이 안되기 때문에
+        if (accountRepository != null) {
+            accountRepository.save(withdrawAccount);
+            accountRepository.save(depositAccount);
+        }
+        Transaction transaction = Transaction.builder()
+                .withdrawAccount(withdrawAccount)
+                .depositAccount(depositAccount)
+                .withdrawAccountBalance(withdrawAccount.getBalance())
+                .depositAccountBalance(depositAccount.getBalance())
+                .amount(100L)
+                .gubun(TransactionEnum.TRANSFER)
+                .sender(withdrawAccount.getNumber() + "")
+                .receiver(depositAccount.getNumber() + "")
+                .build();
         return transaction;
     }
 }
--- src/main/java/shop/mtcoding/bank/domain/transaction/TransactionRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/transaction/TransactionRepository.java
@@ -2,5 +2,5 @@ package shop.mtcoding.bank.domain.transaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long>, Dao {
 }
